ee8047e7e2a263e851b017470a7d70dbabc53c64
algeoba <73835756+algeoba@users.noreply.github.com>
2022-06-10 20:47:19 +0300

    Update SmartcatBookmarklets.js

--- SmartcatBookmarklets.js
+++ SmartcatBookmarklets.js
@@ -16,7 +16,8 @@ javascript: (function () {
 				'out', 'outside', 'over', 'per', 'regarding', 'respecting', 'short', 'since', 'than', 'the', 'through', 'throughout', 
 				'till', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike', 'until', 'unto', 'up', 'upon', 'v.', 'versus', 'via', 
 				'vs.', 'with', 'within', 'without'];
-	let selectionRemade = selectionArray.map(item => (noCapitalization.includes(item) == true) ? item = item : item = item[0].toUpperCase() + item.slice(1)).join(' ');
+	let selectionRemade = selectionArray.map(item => (noCapitalization.includes(item) == true) ? 
+						 item = item : item = item[0].toUpperCase() + item.slice(1)).join(' ');
 	let output = currentSegment.innerText.replace(selectionString, selectionRemade);
 	currentSegment.innerText = output;
 })()
@@ -32,7 +33,8 @@ javascript: (function () {
 	let currentSegment = document.activeElement;
 	let currentSegmentText = currentSegment.innerText;
 	let selectionString = ' ' + window.getSelection().toString();
-	let output = selectionString.substr(1, 1).toUpperCase() + selectionString.slice(2) + ', ' + currentSegmentText.substr(0, 1).toLowerCase() + currentSegmentText.slice(1).replace(selectionString, '');
+	let output = selectionString.substr(1, 1).toUpperCase() + selectionString.slice(2) + ', ' + 
+	    currentSegmentText.substr(0, 1).toLowerCase() + currentSegmentText.slice(1).replace(selectionString, '');
 	currentSegment.innerText = output;
 })()
 
@@ -47,7 +49,8 @@ javascript: (function () {
 	navigator.clipboard.readText().then(clipboardText => {
 		clipboardArray = clipboardText.split(/[\r\n]+/g);
 		for (i = 0; i < clipboardArray.length; i++) {
-			statement = 'document.activeElement.closest(&quot;tbody&quot;)' + '.nextElementSibling'.repeat(i) + '.getElementsByClassName(&quot;l-content-editor__view l-content-editor__view_editor&quot;)[1].innerText=clipboardArray[i]';
+			statement = 'document.activeElement.closest(&quot;tbody&quot;)' + '.nextElementSibling'.repeat(i) + 
+				'.getElementsByClassName(&quot;l-content-editor__view l-content-editor__view_editor&quot;)[1].innerText=clipboardArray[i]';
 			eval(statement);
 		}
 	})
@@ -80,7 +83,8 @@ javascript: (function () {
 	let currentSegment = document.activeElement;
 	let currentSegmentText = currentSegment.innerText;
 	let selectionString = window.getSelection().toString();
-	let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 
+		      'September', 'October', 'November', 'December'];
 	let selectionArray = selectionString.split('.');
 	let day;
 	(selectionArray[0].split(0, 1) == 0) ? day = selectionArray[0].substr(1, 1) : day = selectionArray[0];
@@ -99,7 +103,8 @@ javascript: (function () {
 	let currentSegment = document.activeElement;
 	let currentSegmentText = currentSegment.innerText;
 	let selectionString = window.getSelection().toString();
-	let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+	let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
+		      'August', 'September', 'October', 'November', 'December'];
 	let selectionArray = selectionString.split('.');
 	let day;
 	(selectionArray[0].split(0, 1) == 0) ? day = selectionArray[0].substr(1, 1) : day = selectionArray[0];
